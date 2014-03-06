3c1141abe52a4969207bc145136b6566051f4b07
Pascal <pascal@unixadmins.su>
2014-03-06 22:21:34 +0400

    add qemu_manage.h

---
+++ qemu_manage.h
@@ -0,0 +1,15 @@
+#ifndef QEMU_MANAGE_H_
+#define QEMU_MANAGE_H_
+
+#include <string>
+#include <fstream>
+
+#include <boost/regex.hpp>
+
+class ParseCfg {
+  public:
+    std::string param;
+  private:
+};
+
+#endif
