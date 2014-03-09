ace2c23e1647fea6fff45e01daf8b353ad1a8e89
Pascal <pascal@unixadmins.su>
2014-03-10 01:33:05 +0400

    Fix clear screen when exit

--- qemu_manage.cpp
+++ qemu_manage.cpp
@@ -56,6 +56,8 @@ int main() {
           choice = highlight;
           break;
         case KEY_F(10):
+          clear();
+          refresh();
           endwin();
           exit(10);
           break;
