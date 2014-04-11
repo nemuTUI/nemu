3ba6fef7bc52316fb66f3a884ff888cfd2bab79f
Pascal <pascal@unixadmins.su>
2014-04-11 13:55:50 +0400

    Increase version

--- qemu_manage.h
+++ qemu_manage.h
@@ -14,7 +14,7 @@
 #include <boost/property_tree/ini_parser.hpp>
 
 #define _(str) gettext(str)
-#define VERSION "0.1.0"
+#define VERSION "0.1.1"
 
 namespace QManager {
   extern std::atomic<bool> finish;
