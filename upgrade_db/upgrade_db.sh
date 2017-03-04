36acfc13ab5ac329d966c6b34125ce4ededb144e
Void <rootpascal@gmail.com>
2017-03-04 23:22:24 +0300

    More fix to database upgrade script

--- upgrade_db/upgrade_db.sh
+++ upgrade_db/upgrade_db.sh
@@ -6,23 +6,34 @@ if [ -z "$1" ]; then
 fi
 
 DB_PATH="$1"
-DB_VERSION=$(sqlite3 "$DB_PATH" -line 'PRAGMA user_version;' | sed 's/.*\s=\s//')
+DB_ACTUAL_VERSION=1
+DB_CURRENT_VERSION=$(sqlite3 "$DB_PATH" -line 'PRAGMA user_version;' | sed 's/.*\s=\s//')
 RC=0
 
-echo "database version: ${DB_VERSION}"
+if [ "$DB_CURRENT_VERSION" = "$DB_ACTUAL_VERSION" ]; then
+    echo "No need to upgrade."
+    exit 0
+fi
 
-case "$DB_VERSION" in
-    ( 0 )
-        (
-        sqlite3 "$DB_PATH" -line 'ALTER TABLE vms ADD mouse_override_1 integer;' &&
-        sqlite3 "$DB_PATH" -line 'UPDATE vms SET mouse_override_1 = 42 WHERE mouse_override IS NULL;' &&
-        sqlite3 "$DB_PATH" -line 'PRAGMA user_version=1'
-        ) || RC=1
-        ;;
+echo "database version: ${DB_CURRENT_VERSION}"
+while [ "$DB_CURRENT_VERSION" != "$DB_ACTUAL_VERSION" ]; do
+    [ "$RC" = 1 ] && break;
 
-    ( * )
-        echo "Unsupported database user_version" >&2
-        exit 1
-esac
+    case "$DB_CURRENT_VERSION" in
+        ( 0 )
+            (
+            sqlite3 "$DB_PATH" -line 'ALTER TABLE vms ADD mouse_override integer;' &&
+            sqlite3 "$DB_PATH" -line 'UPDATE vms SET mouse_override = 0 WHERE mouse_override IS NULL;' &&
+            sqlite3 "$DB_PATH" -line 'PRAGMA user_version=1'
+            ) || RC=1
+            ;;
 
-[ "$RC" = 0 ] && echo "done"
+        ( * )
+            echo "Unsupported database user_version" >&2
+            exit 1
+    esac
+    
+    DB_CURRENT_VERSION=$(sqlite3 "$DB_PATH" -line 'PRAGMA user_version;' | sed 's/.*\s=\s//')
+done
+
+[ "$RC" = 0 ] && echo "database upgrade complete."
