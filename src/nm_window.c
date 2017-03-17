f58ab6c970bbef0aab67b1a3218994477f097fb0
Pascal <pascal@unixadmins.su>
2017-03-17 10:56:51 +0300

    nm_clear_screen function

--- src/nm_window.c
+++ src/nm_window.c
@@ -3,6 +3,8 @@
 #define NM_MAIN_MSG "Use arrow keys to go up and down, "\
     "Press enter to select a choice, F10 - exit"
 
+static inline void nm_clear_screen(void);
+
 void nm_print_main_window(void)
 {
     int col;
@@ -12,10 +14,20 @@ void nm_print_main_window(void)
     col = getmaxx(stdscr);
     msg_len = mbstowcs(NULL, help_msg, strlen(help_msg));
 
-    clear();
+    nm_clear_screen();
     border(0,0,0,0,0,0,0,0);
     mvprintw(1, (col - msg_len) / 2, help_msg);
     refresh();
+}
+
+static inline void nm_clear_screen(void)
+{
+    int x, y;
+
+    getmaxyx(stdscr, x, y);
+
+    for (int i = 0; i <= y; i++)
+        mvhline(i, 0, ' ', x);
 }
 
 /* vim:set ts=4 sw=4 fdm=marker: */
