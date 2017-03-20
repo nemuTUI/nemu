6f492f6bee8909476bb561cc3e3acbc416aec3e5
Pascal <pascal@unixadmins.su>
2017-03-20 11:49:17 +0300

    ini parser done

--- src/nm_ini_parser.c
+++ src/nm_ini_parser.c
@@ -25,7 +25,11 @@ void *nm_ini_parser_init(const nm_str_t *path)
     char *buf_ini;
     int look_for_sec_end = 0;
     int look_for_param_end = 0;
-    nm_str_t buf_value = NM_INIT_STR;
+    int look_for_value_end = 0;
+    int comment_block = 0;
+    nm_str_t sec_name = NM_INIT_STR;
+    nm_str_t par_name = NM_INIT_STR;
+    nm_str_t par_value = NM_INIT_STR;
     nm_file_map_t file = NM_INIT_FILE;
     nm_ini_node_t *head = NULL;
     nm_ini_node_t *curr_node;
@@ -35,34 +39,74 @@ void *nm_ini_parser_init(const nm_str_t *path)
     nm_map_file(&file);
     buf_ini = file.mp;
 
-    for (off_t n = 0; n < file.size; n++)
+    for (off_t n = 0; n < file.size; n++, buf_ini++)
     {
-        if (look_for_param_end)
+        /* skip spaces */
+        if (*buf_ini == 0x20)
+            continue;
+
+        if (*buf_ini == '[')
         {
-            if (*buf_ini == '=')
-            {
-                //
-            }
+            look_for_sec_end = 1;
+            continue;
         }
 
+        /* {{{ skip comments '#...' */
+        if (comment_block && (*buf_ini != '\n'))
+            continue;
+
+        if (*buf_ini == 0x23)
+        {
+            comment_block = 1;
+            continue;
+        }
+        if (comment_block && (*buf_ini == '\n'))
+        {
+            comment_block = 0;
+            continue;
+        } /* }}} comments */
+
+        /* {{{ collect section names */
         if (look_for_sec_end)
         {
             if (*buf_ini == ']')
             {
-                curr_node = nm_ini_node_push(&head, &buf_value);
-                nm_str_free(&buf_value);
+                curr_node = nm_ini_node_push(&head, &sec_name);
+                nm_str_free(&sec_name);
                 look_for_sec_end = 0;
                 look_for_param_end = 1;
-                buf_ini++;
                 continue;
             }
-            nm_str_add_char_opt(&buf_value, *buf_ini);
-        }
-        
-        if (*buf_ini == '[')
-            look_for_sec_end = 1;
+            nm_str_add_char_opt(&sec_name, *buf_ini);
+        } /* }}} section names */
 
-        buf_ini++;
+        /* {{{ collect param names */
+        if (look_for_param_end && !look_for_sec_end)
+        {
+            if (*buf_ini == '=')
+            {
+                look_for_param_end = 0;
+                look_for_value_end = 1;
+                continue;
+            }
+            if (*buf_ini != '\n')
+                nm_str_add_char_opt(&par_name, *buf_ini);
+        } /* }}} param */
+
+        /* {{{ collect values */
+        if (look_for_value_end)
+        {
+            if (*buf_ini == '\n')
+            {
+                look_for_value_end = 0;
+                look_for_param_end = 1;
+                nm_ini_value_push(curr_node, &par_name, &par_value);
+                nm_str_free(&par_name);
+                nm_str_free(&par_value);
+                continue;
+            }
+            nm_str_add_char_opt(&par_value, *buf_ini);
+        } /* }}} */
     }
 
     nm_unmap_file(&file);
@@ -82,7 +126,15 @@ void nm_ini_parser_dump(void *head)
 
     while (curr != NULL)
     {
+        nm_ini_value_t *values = curr->values_head;
+
         printf("section: %s\n", curr->name.data);
+        while (values != NULL)
+        {
+            printf("\t%s = %s\n", values->name.data, values->value.data);
+            values = values->next;
+        }
+
         curr = curr->next;
     }
 }
@@ -97,6 +149,16 @@ void nm_ini_parser_free(void *head)
     while (curr != NULL)
     {
         nm_ini_node_t *old = curr;
+        nm_ini_value_t *values = curr->values_head;
+
+        while (values != NULL)
+        {
+            nm_ini_value_t *old_val = values;
+            nm_str_free(&values->name);
+            nm_str_free(&values->value);
+            values = values->next;
+            free(old_val);
+        }
 
         nm_str_free(&curr->name);
         curr = curr->next;
@@ -129,7 +191,29 @@ fill_data:
 
 static void nm_ini_value_push(nm_ini_node_t *head, nm_str_t *name, nm_str_t *value)
 {
-    //
+    nm_ini_value_t *curr;
+
+    if (head == NULL)
+        nm_bug(_("NULL list head pointer"));
+
+    curr = head->values_head;
+
+    if (curr == NULL) /* list is empty */
+    {
+        curr = nm_calloc(1, sizeof(nm_ini_value_t));
+        head->values_head = curr;
+        goto fill_data;
+    }
+
+    while (curr->next != NULL)
+        curr = curr->next;
+
+    curr->next = nm_calloc(1, sizeof(nm_ini_value_t));
+    curr = curr->next;
+
+fill_data:
+    nm_str_copy(&curr->name, name);
+    nm_str_copy(&curr->value, value);
 }
 
 /* vim:set ts=4 sw=4 fdm=marker: */
