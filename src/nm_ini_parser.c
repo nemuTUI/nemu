963d8ba58d09f95e31ec28b6f4f77aaae4d7c99a
Void <rootpascal@gmail.com>
2017-03-20 09:58:50 +0300

    More work on ini parser

--- src/nm_ini_parser.c
+++ src/nm_ini_parser.c
@@ -2,53 +2,67 @@
 #include <nm_utils.h>
 #include <nm_string.h>
 
+typedef struct nm_ini_value_t nm_ini_value_t;
 typedef struct nm_ini_node_t nm_ini_node_t;
 
-typedef struct {
+struct nm_ini_value_t {
     nm_str_t name;
     nm_str_t value;
-} nm_ini_value_t;
+    nm_ini_value_t *next;
+};
 
 struct nm_ini_node_t {
     nm_str_t name;
     nm_ini_node_t *next;
-    nm_ini_value_t *values;
+    nm_ini_value_t *values_head;
 };
 
-static void nm_ini_list_push(nm_ini_node_t **head, nm_str_t *name);
+static nm_ini_node_t *nm_ini_node_push(nm_ini_node_t **head, nm_str_t *name);
+static void nm_ini_value_push(nm_ini_node_t *head, nm_str_t *name, nm_str_t *value);
 
 void *nm_ini_parser_init(const nm_str_t *path)
 {
-    char *buf;
+    char *buf_ini;
     int look_for_sec_end = 0;
-    nm_str_t sec_name = NM_INIT_STR;
+    int look_for_param_end = 0;
+    nm_str_t buf_value = NM_INIT_STR;
     nm_file_map_t file = NM_INIT_FILE;
     nm_ini_node_t *head = NULL;
+    nm_ini_node_t *curr_node;
 
     file.name = path;
 
     nm_map_file(&file);
-    buf = file.mp;
+    buf_ini = file.mp;
 
     for (off_t n = 0; n < file.size; n++)
     {
+        if (look_for_param_end)
+        {
+            if (*buf_ini == '=')
+            {
+                //
+            }
+        }
+
         if (look_for_sec_end)
         {
-            if (*buf == ']')
+            if (*buf_ini == ']')
             {
-                nm_ini_list_push(&head, &sec_name);
-                nm_str_free(&sec_name);
+                curr_node = nm_ini_node_push(&head, &buf_value);
+                nm_str_free(&buf_value);
                 look_for_sec_end = 0;
-                buf++;
+                look_for_param_end = 1;
+                buf_ini++;
                 continue;
             }
-            nm_str_add_char_opt(&sec_name, *buf);
+            nm_str_add_char_opt(&buf_value, *buf_ini);
         }
         
-        if (*buf == '[')
+        if (*buf_ini == '[')
             look_for_sec_end = 1;
 
-        buf++;
+        buf_ini++;
     }
 
     nm_unmap_file(&file);
@@ -76,21 +90,21 @@ void nm_ini_parser_dump(void *head)
 void nm_ini_parser_free(void *head)
 {
     nm_ini_node_t *curr = head;
-    nm_ini_node_t *old;
 
     if (curr == NULL)
         return;
 
     while (curr != NULL)
     {
+        nm_ini_node_t *old = curr;
+
         nm_str_free(&curr->name);
-        old = curr;
         curr = curr->next;
         free(old);
     }
 }
 
-static void nm_ini_list_push(nm_ini_node_t **head, nm_str_t *name)
+static nm_ini_node_t *nm_ini_node_push(nm_ini_node_t **head, nm_str_t *name)
 {
     nm_ini_node_t *curr = *head;
 
@@ -109,6 +123,13 @@ static void nm_ini_list_push(nm_ini_node_t **head, nm_str_t *name)
 
 fill_data:
     nm_str_copy(&curr->name, name);
+
+    return curr;
+}
+
+static void nm_ini_value_push(nm_ini_node_t *head, nm_str_t *name, nm_str_t *value)
+{
+    //
 }
 
 /* vim:set ts=4 sw=4 fdm=marker: */
