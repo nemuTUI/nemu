280faf669d2b1b496b31136d5ab82d9627829f92
Pascal <pascal@unixadmins.su>
2017-03-21 15:09:31 +0300

    cfg: add ini macro

--- src/nm_cfg_file.c
+++ src/nm_cfg_file.c
@@ -30,7 +30,7 @@ void nm_cfg_init(void)
 #if (NM_DEBUG)
     nm_ini_parser_dump(ini);
 #endif
-    nm_get_param(ini, "main", "vmdir", &cfg.vm_dir);
+    nm_get_param(ini, NM_INI_S_MAIN, NM_INI_P_VM, &cfg.vm_dir);
     if (stat(cfg.vm_dir.data, &file_info) == -1)
         nm_bug("cfg: %s: %s", cfg.vm_dir.data, strerror(errno));
     if ((file_info.st_mode & S_IFMT) != S_IFDIR)
@@ -38,13 +38,13 @@ void nm_cfg_init(void)
     if (access(cfg.vm_dir.data, W_OK) != 0)
         nm_bug(_("cfg: no write access to %s"), cfg.vm_dir.data);
 
-    nm_get_param(ini, "main", "db", &cfg.db_path);
+    nm_get_param(ini, NM_INI_S_MAIN, NM_INI_P_DB, &cfg.db_path);
     nm_str_dirname(&cfg.db_path, &tmp_buf);
     if (access(tmp_buf.data, W_OK) != 0)
         nm_bug(_("cfg: no write access to %s"), tmp_buf.data);
     nm_str_trunc(&tmp_buf, 0);
 
-    nm_get_param(ini, "main", "list_max", &tmp_buf);
+    nm_get_param(ini, NM_INI_S_MAIN, NM_INI_P_LIST, &tmp_buf);
     cfg.list_max = nm_str_stoui(&tmp_buf);
     if ((cfg.list_max == 0) || (cfg.list_max > 100))
     {
@@ -52,7 +52,7 @@ void nm_cfg_init(void)
             cfg.list_max);
     }
 
-    nm_get_param(ini, "vnc", "binary", &cfg.vnc_bin);
+    nm_get_param(ini, NM_INI_S_VNC, NM_INI_P_VBIN, &cfg.vnc_bin);
     if (stat(cfg.vnc_bin.data, &file_info) == -1)
         nm_bug("cfg: %s: %s", cfg.vnc_bin.data, strerror(errno));
 
--- src/nm_cfg_file.h
+++ src/nm_cfg_file.h
@@ -7,6 +7,19 @@
 #define NM_DEFAULT_VNC    "/usr/bin/vncviewer"
 #define NM_DEFAULT_TARGET "x86_64,i386"
 
+#define NM_INI_S_MAIN   "main"
+#define NM_INI_S_VNC    "vnc"
+#define NM_INI_S_QEMU   "qemu"
+
+#define NM_INI_P_VM     "vmdir"
+#define NM_INI_P_DB     "db"
+#define NM_INI_P_LIST   "list_max"
+#define NM_INI_P_VBIN   "binary"
+#define NM_INI_P_VANY   "listen_any"
+#define NM_INI_P_QBIN   "qemu_system_path"
+#define NM_INI_P_QTRG   "targets"
+#define NM_INI_P_QLOG   "log_cmd"
+
 typedef struct {
     nm_str_t vm_dir;
     nm_str_t db_path;
