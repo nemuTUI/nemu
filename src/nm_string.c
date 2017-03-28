719ae6920ccb679724db3b559d6a58662d4b227e
Void <rootpascal@gmail.com>
2017-03-28 08:58:36 +0300

    string: add nm_str_format()

--- src/nm_string.c
+++ src/nm_string.c
@@ -141,6 +141,34 @@ void nm_str_dirname(const nm_str_t *str, nm_str_t *res)
     nm_str_alloc_mem(res, data, pos);
 }
 
+void nm_str_format(nm_str_t *str, const char *fmt, ...)
+{
+    int len = 0;
+    char *buf = NULL;
+    va_list args;
+
+    /* get required length */
+    va_start(args, fmt);
+    len = vsnprintf(buf, len, fmt, args);
+    va_end(args);
+
+    if (len < 0)
+        nm_bug(_("%s: invalid length: %d"), __func__, len);
+
+    len++; /* for \0 */
+
+    buf = nm_calloc(1, len);
+
+    va_start(args, fmt);
+    len = vsnprintf(buf, len, fmt, args);
+    if (len < 0)
+        nm_bug(_("%s: invalid length: %d"), __func__, len);
+    va_end(args);
+
+    nm_str_append_mem(str, buf, len);
+    free(buf);
+}
+
 static void nm_str_alloc_mem(nm_str_t *str, const char *src, size_t len)
 {
     size_t len_needed;
--- src/nm_string.h
+++ src/nm_string.h
@@ -24,6 +24,8 @@ uint64_t nm_str_stoul(const nm_str_t *str);
 int nm_str_cmp_st(const nm_str_t *str, const char *text);
 int nm_str_cmp_tt(const char *text1, const char *text2);
 int nm_str_cmp_ss(const nm_str_t *str1, const nm_str_t *str2);
+void nm_str_format(nm_str_t *str, const char *fmt, ...)
+    __attribute__ ((format(printf, 2, 3)));
 
 #endif /* NM_STRING_H_ */
 /* vim:set ts=4 sw=4 fdm=marker: */
