2a4983eab3bb3bf0f1e01e69fcefb5e6d4bdb3f2
Void <rootpascal@gmail.com>
2017-05-04 19:27:15 +0300

    qmp: add files

---
+++ src/nm_qmp_control.c
@@ -0,0 +1,5 @@
+#include <nm_core.h>
+
+//...
+
+/* vim:set ts=4 sw=4 fdm=marker: */
---
+++ src/nm_qmp_control.h
@@ -0,0 +1,5 @@
+#ifndef NM_QMP_CONTROL_H_
+#define NM_QMP_CONTROL_H_
+
+#endif /* NM_QMP_CONTROL_H_ */
+/* vim:set ts=4 sw=4 fdm=marker: */
