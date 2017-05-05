6c0ba13de8ca0d3028ee34b06e99873b3e52e078
Void <rootpascal@gmail.com>
2017-05-05 08:28:06 +0300

    qmp: add commands

--- src/nm_qmp_control.c
+++ src/nm_qmp_control.c
@@ -1,5 +1,6 @@
 #include <nm_core.h>
 
-//...
+#define NM_QMP_CMD_INIT    "{\"execute\":\"qmp_capabilities\"}"
+#define NM_QMP_CMD_VM_SHUT "{\"execute\":\"system_powerdown\"}"
 
 /* vim:set ts=4 sw=4 fdm=marker: */
