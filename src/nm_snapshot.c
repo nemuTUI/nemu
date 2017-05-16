aa1b99f316cf0976193d7280e2043b35647187dc
Pascal <pascal@unixadmins.su>
2017-05-16 17:15:49 +0300

    snapshot: more work

--- src/nm_snapshot.c
+++ src/nm_snapshot.c
@@ -18,9 +18,56 @@ void nm_snapshot_create(const nm_str_t *name)
     nm_window_t *window = NULL;
     nm_field_t *fields[NM_SNAP_FIELDS_NUM + 1];
     nm_spinner_data_t sp_data = NM_INIT_SPINNER;
+    nm_str_t buf = NM_INIT_STR;
     size_t msg_len;
     pthread_t spin_th;
     int done = 0;
+
+    nm_print_title(_(NM_EDIT_TITLE));
+    window = nm_init_window(9, 45, 3);
+
+    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+    wbkgd(window, COLOR_PAIR(1));
+
+    for (size_t n = 0; n < NM_SNAP_FIELDS_NUM; ++n)
+    {
+        fields[n] = new_field(1, 19, (n + 1) * 2, 1, 0, 0);
+        set_field_back(fields[n], A_UNDERLINE);
+    }
+
+    fields[NM_SNAP_FIELDS_NUM] = NULL;
+
+    /*set_field_type(fields[NM_FLD_DRVSIZE], TYPE_INTEGER, 0, 1, nm_hw_disk_free());
+    set_field_type(fields[NM_FLD_DRVTYPE], TYPE_ENUM, nm_form_drive_drv, false, false);
+
+    set_field_buffer(fields[NM_FLD_DRVTYPE], 0, NM_DEFAULT_DRVINT);*/
+
+    nm_str_alloc_text(&buf, _("Snapshot "));
+    nm_str_add_str(&buf, name);
+    mvwaddstr(window, 1, 2, buf.data);
+    mvwaddstr(window, 4, 2, _("Drive"));
+    mvwaddstr(window, 6, 2, _("Snapshot name"));
+
+    form = nm_post_form(window, fields, 22);
+    if (nm_draw_form(window, form) != NM_OK)
+        goto out;
+
+    msg_len = mbstowcs(NULL, _(NM_EDIT_TITLE), strlen(_(NM_EDIT_TITLE)));
+    sp_data.stop = &done;
+    sp_data.x = (getmaxx(stdscr) + msg_len + 2) / 2;
+
+    if (pthread_create(&spin_th, NULL, nm_spinner, (void *) &sp_data) != 0)
+        nm_bug(_("%s: cannot create thread"), __func__);
+
+    /*XXX action here */
+
+    done = 1;
+    if (pthread_join(spin_th, NULL) != 0)
+        nm_bug(_("%s: cannot join thread"), __func__);
+
+out:
+    nm_form_free(form, fields);
+    nm_str_free(&buf);
 }
 
 /* vim:set ts=4 sw=4 fdm=marker: */
