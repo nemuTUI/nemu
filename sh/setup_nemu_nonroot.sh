507a8212d392744bd1bea38245ef177c05260f55
Void <rootpascal@gmail.com>
2017-05-26 08:48:20 +0300

    install: add setup for non root usage script

---
+++ sh/setup_nemu_nonroot.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <linux|freebsd> <username>"
+    exit 0
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Run me as root" >&2
+    exit 1
+fi
+
+KVM_GROUP=""
+USB_GROUP=""
+OS="$1"
+USER="$2"
+
+case "$OS" in
+    ( linux )
+        KVM_GROUP=$(ls -la /dev/kvm | cut -d ' ' -f 4)
+        if [ "$KVM_GROUP" = "root" ]; then
+            echo "Additional group for KVM device is missing" >&2
+            exit 1
+        fi
+        
+        USB_GROUP=$(ls -la /dev/bus/usb/001/001 | cut -d ' ' -f 4)
+        if [ "$USB_GROUP" = "root" ]; then
+            echo "Additional group for USB devices is missing" >&2
+            exit 1
+        fi
+        
+        gpasswd -a $USER $KVM_GROUP && \
+        gpasswd -a $USER $USB_GROUP && \
+        ls -1 /usr/bin/qemu-system-* | xargs -n1 setcap CAP_NET_ADMIN=ep && \
+        setcap CAP_NET_ADMIN=ep /usr/bin/nemu && \
+        echo "[OK]"
+        ;;
+    
+    ( * )
+        echo "Unsupported" >&2
+        ;;
+esac
