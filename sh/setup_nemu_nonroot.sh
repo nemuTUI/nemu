f6895c8b688ccd081f56f791b1a2034ac6b826e1
Pascal <pascal@unixadmins.su>
2017-05-30 13:47:02 +0300

    setup: fix setup-non-root

--- sh/setup_nemu_nonroot.sh
+++ sh/setup_nemu_nonroot.sh
@@ -19,23 +19,24 @@ case "$OS" in
     ( linux )
         KVM_GROUP=$(ls -la /dev/kvm | cut -d ' ' -f 4)
         if [ "$KVM_GROUP" = "root" ]; then
-            echo "Additional group for KVM device is missing" >&2
-            exit 1
+            echo "Warning: Additional group for KVM device is missing" >&2
+            echo "Fix it and run script again, \"-enable-kvm\" will not work" >&2
+        else
+          if ! id -nG $USER | grep -qw $KVM_GROUP; then
+            gpasswd -a $USER $KVM_GROUP
+            [ "$?" -ne 0 ] && echo "[ERR]" && exit 1
+          fi
         fi
 
         USB_GROUP=$(ls -la /dev/bus/usb/001/001 | cut -d ' ' -f 4)
         if [ "$USB_GROUP" = "root" ]; then
-            echo "Additional group for USB devices is missing" >&2
-            exit 1
-        fi
-
-        if ! id -nG $USER | grep -qw $KVM_GROUP; then
-          gpasswd -a $USER $KVM_GROUP
-          [ "$?" -ne 0 ] && echo "[ERR]" && exit 1
-        fi
-        if ! id -nG $USER | grep -qw $USB_GROUP; then
-          gpasswd -a $USER $USB_GROUP
-          [ "$?" -ne 0 ] && echo "[ERR]" && exit 1
+            echo "Warning: additional group for USB devices is missing" >&2
+            echo "Fix it and run script again, \"-usb\" will not work" >&2
+        else
+          if ! id -nG $USER | grep -qw $USB_GROUP; then
+            gpasswd -a $USER $USB_GROUP
+            [ "$?" -ne 0 ] && echo "[ERR]" && exit 1
+          fi
         fi
 
         ls -1 /usr/bin/qemu-system-* | xargs -n1 setcap CAP_NET_ADMIN=ep && \
