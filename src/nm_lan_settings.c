10f5063c29f91bfcb2190ef4211198125cb03d67
Pascal <pascal@unixadmins.su>
2017-06-27 18:27:44 +0300

    lan: add files

---
+++ src/nm_lan_settings.c
@@ -0,0 +1,2 @@
+#include <nm_core.h>
+/* vim:set ts=4 sw=4 fdm=marker: */
---
+++ src/nm_lan_settings.h
@@ -0,0 +1,5 @@
+#ifndef NM_LAN_SETTINGS_H_
+#define NM_LAN_SETTINGS_H_
+
+#endif /* NM_LAN_SETTINGS_H_ */
+/* vim:set ts=4 sw=4 fdm=marker: */
