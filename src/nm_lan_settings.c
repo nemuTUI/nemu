a3c66a9528094d12541a0c87181b0a81809400aa
Pascal <pascal@unixadmins.su>
2017-06-28 20:25:40 +0300

    lan: menu scroll

--- src/nm_lan_settings.c
+++ src/nm_lan_settings.c
@@ -16,20 +16,30 @@ static void nm_lan_del_veth(const nm_str_t *name);
 
 void nm_lan_settings(void)
 {
-    int ch = 0;
+    int ch = 0, regen_data = 1;
     nm_str_t query = NM_INIT_STR;
     nm_window_t *veth_window = NULL;
+    nm_vect_t veths = NM_INIT_VECT;
+    nm_vect_t veths_list = NM_INIT_VECT;
+    nm_menu_data_t veths_data = NM_INIT_MENU_DATA;
+    uint32_t list_max = 0;
 
     nm_str_alloc_text(&query, NM_LAN_GET_VETH_SQL);
 
     keypad(stdscr, 1);
 
+    nm_db_select(query.data, &veths);
+
     for (;;)
     {
-        nm_vect_t veths =  NM_INIT_VECT;
-        nm_menu_data_t veths_data = NM_INIT_MENU_DATA;
-        nm_db_select(query.data, &veths);
         nm_print_vm_window();
+
+        if (regen_data)
+        {
+            nm_vect_free(&veths_list, NULL);
+            nm_vect_free(&veths, nm_str_vect_free_cb);
+            nm_db_select(query.data, &veths);
+        }
 
         if (veths.n_memb == 0)
         {
@@ -41,12 +51,11 @@ void nm_lan_settings(void)
             msg_len = mbstowcs(NULL, msg, strlen(msg));
             mvprintw(4, (col - msg_len) / 2, msg);
         }
-        else
-        {
-            uint32_t list_max = nm_cfg_get()->list_max;
-            nm_vect_t veths_list = NM_INIT_VECT;
 
+        if (regen_data)
+        {
             veths_data.highlight = 1;
+            list_max = nm_cfg_get()->list_max;
 
             if (list_max > veths.n_memb)
                 list_max = veths.n_memb;
@@ -69,31 +78,88 @@ void nm_lan_settings(void)
             }
             veth_window = nm_init_window(list_max + 4, 40, 7);
             nm_print_veth_menu(veth_window, &veths_data);
+            regen_data = 0;
+        }
+        else
+        {
+            veth_window = nm_init_window(list_max + 4, 40, 7);
+            nm_print_veth_menu(veth_window, &veths_data);
         }
 
-        switch (ch = getch()) {
-        case NM_KEY_A:
-            nm_lan_add_veth();
-            break;
+        ch = getch();
 
-        case NM_KEY_R:
+        if ((veths.n_memb > 0) && (ch == KEY_UP) && (veths_data.highlight == 1) &&
+            (veths_data.item_first == 0) && (list_max < veths_data.v->n_memb))
+        {
+            veths_data.highlight = list_max;
+            veths_data.item_first = veths_data.v->n_memb - list_max;
+            veths_data.item_last = veths_data.v->n_memb;
+        }
+
+        else if ((veths.n_memb > 0) && (ch == KEY_UP))
+        {
+            if ((veths_data.highlight == 1) && (veths_data.v->n_memb <= list_max))
+                veths_data.highlight = veths_data.v->n_memb;
+            else if ((veths_data.highlight == 1) && (veths_data.item_first != 0))
+            {
+                veths_data.item_first--;
+                veths_data.item_last--;
+            }
+            else
+            {
+                veths_data.highlight--;
+            }
+        }
+
+        else if ((veths.n_memb > 0) && (ch == KEY_DOWN) &&
+                 (veths_data.highlight == list_max) &&
+                 (veths_data.item_last == veths_data.v->n_memb))
+        {
+            veths_data.highlight = 1;
+            veths_data.item_first = 0;
+            veths_data.item_last = list_max;
+        }
+
+        else if ((veths.n_memb > 0) && (ch == KEY_DOWN))
+        {
+            if ((veths_data.highlight == veths_data.v->n_memb) &&
+                (veths_data.v->n_memb <= list_max))
+                veths_data.highlight = 1;
+            else if ((veths_data.highlight == list_max) &&
+                     (veths_data.item_last < veths_data.v->n_memb))
+            {
+                veths_data.item_first++;
+                veths_data.item_last++;
+            }
+            else
+            {
+                veths_data.highlight++;
+            }
+        }
+
+        else if (ch == NM_KEY_A)
+        {
+            nm_lan_add_veth();
+            regen_data = 1;
+        }
+
+        else if (ch == NM_KEY_R)
+        {
             if (veths.n_memb > 0)
                 nm_lan_del_veth(nm_vect_item_name_cur(veths_data));
-            break;
-
-        case KEY_F(1):
-            nm_lan_help();
-            break;
-
-        case NM_KEY_ESC:
-            nm_vect_free(&veths, nm_str_vect_free_cb);
-            goto out;
+            regen_data = 1;
         }
 
-        nm_vect_free(&veths, nm_str_vect_free_cb);
+        else if (ch == KEY_F(1))
+            nm_lan_help();
+
+        else if (ch == NM_KEY_ESC)
+            goto out;
     }
 
 out:
+    nm_vect_free(&veths, nm_str_vect_free_cb);
+    nm_vect_free(&veths_list, NULL);
     keypad(stdscr, 0);
     nm_str_free(&query);
 }
@@ -127,8 +193,8 @@ static void nm_lan_add_veth(void)
     nm_str_t r_name = NM_INIT_STR;
     nm_str_t query = NM_INIT_STR;
 
-    nm_str_alloc_text(&l_name, "vm1");
-    nm_str_alloc_text(&r_name, "vm2");
+    nm_str_alloc_text(&l_name, "ololo");
+    nm_str_alloc_text(&r_name, "trololo");
 
     nm_net_add_veth(&l_name, &r_name);
     nm_net_link_up(&l_name);
