8a36a40415db9ae0e139a46b18191371136f475f
Pascal <pascal@unixadmins.su>
2017-07-19 12:18:50 +0300

    9p: add files

---
+++ src/nm_9p_share.c
@@ -0,0 +1,3 @@
+#include <nm_core.h>
+
+/* vim:set ts=4 sw=4 fdm=marker: */
---
+++ src/nm_9p_share.h
@@ -0,0 +1,5 @@
+#ifndef NM_9P_SHARE_H_
+#define NM_9P_SHARE_H_
+
+#endif /* NM_9P_SHARE_H_ */
+/* vim:set ts=4 sw=4 fdm=marker: */
