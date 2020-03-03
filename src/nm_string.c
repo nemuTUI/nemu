nemuTUI/nemu

commit c218ea35e8b67d4adc1d66d3adb0b064feecacfe

nm_database: removed table lastval

src/nm_string.c
@@ -269,7 +269,7 @@ void nm_str_append_format(nm_str_t *str, const char *fmt, ...)
 
     /* get required length */
     va_start(args, fmt);
-    len = vsnprintf(buf, len, fmt, args);
+    len = vsnprintf(buf, 0, fmt, args);
     va_end(args);
 
     if (len < 0)
@@ -280,7 +280,7 @@ void nm_str_append_format(nm_str_t *str, const char *fmt, ...)
     buf = nm_calloc(1, len);
 
     va_start(args, fmt);
-    len = vsnprintf(buf, len, fmt, args);
+    len = vsnprintf(buf, 0, fmt, args);
     if (len < 0)
         nm_bug(_("%s: invalid length: %d"), __func__, len);
     va_end(args);
@@ -297,7 +297,7 @@ void nm_str_format(nm_str_t *str, const char *fmt, ...)
 
     /* get required length */
     va_start(args, fmt);
-    len = vsnprintf(buf, len, fmt, args);
+    len = vsnprintf(buf, 0, fmt, args);
     va_end(args);
 
     if (len < 0)

src/nm_add_vm.c
@@ -104,7 +104,9 @@ static void nm_add_vm_main(int import)
     if (nm_draw_form(action_window, form) != NM_OK)
         goto out;
 
-    nm_form_get_last(&last_mac, &last_vnc);
+    last_mac = nm_form_get_last_mac();
+    last_vnc = nm_form_get_free_vnc();
+
     nm_str_format(&vm.vncp, "%u", last_vnc);
 
     if (nm_add_vm_get_data(&vm, import) != NM_OK)
@@ -330,7 +332,7 @@ void nm_add_vm_to_db(nm_vm_t *vm, uint64_t mac,
         nm_str_t maddr = NM_INIT_STR;
         mac++;
 
-        nm_net_mac_n2a(mac, &maddr);
+        nm_net_mac_n2s(mac, &maddr);
         nm_str_format(&if_name, "%s_eth%zu", vm->name.data, n);
         nm_str_copy(&if_name_copy, &if_name);
         altname = nm_net_fix_tap_name(&if_name, &maddr);
@@ -354,9 +356,6 @@ void nm_add_vm_to_db(nm_vm_t *vm, uint64_t mac,
         nm_str_free(&if_name_copy);
         nm_str_free(&maddr);
     }
-
-    nm_form_update_last_mac(mac);
-    nm_form_update_last_vnc(nm_str_stoui(&vm->vncp, 10));
 
     nm_str_free(&query);
 }

src/nm_clone_vm.c
@@ -142,7 +142,8 @@ static void nm_clone_vm_to_db(const nm_str_t *src, const nm_str_t *dst,
     int altname = 0;
     char drv_ch = 'a';
 
-    nm_form_get_last(&last_mac, &last_vnc);
+    last_mac = nm_form_get_last_mac();
+    last_vnc = nm_form_get_free_vnc();
 
     nm_str_format(&query, NM_CLONE_VMS_SQL, dst->data, last_vnc, src->data);
     nm_db_edit(query.data);
@@ -158,7 +159,7 @@ static void nm_clone_vm_to_db(const nm_str_t *src, const nm_str_t *dst,
         nm_str_t maddr = NM_INIT_STR;
         last_mac++;
 
-        nm_net_mac_n2a(last_mac, &maddr);
+        nm_net_mac_n2s(last_mac, &maddr);
         nm_str_format(&if_name, "%s_eth%zu", dst->data, n);
         nm_str_copy(&if_name_copy, &if_name);
         altname = nm_net_fix_tap_name(&if_name, &maddr);
@@ -197,9 +198,6 @@ static void nm_clone_vm_to_db(const nm_str_t *src, const nm_str_t *dst,
 
         drv_ch++;
     }
-
-    nm_form_update_last_mac(last_mac);
-    nm_form_update_last_vnc(last_vnc);
 
     nm_str_free(&query);
 }

src/nm_database.c
@@ -33,8 +33,6 @@ void nm_db_init(void)
             "socket_path char, initrd char, machine char, fs9p_enable integer, "
             "fs9p_path char, fs9p_name char, usb_type char, spice integer, "
             "debug_port integer, debug_freeze integer, cmdappend char)",
-        "CREATE TABLE lastval(id integer, mac integer, vnc integer)",
-        "INSERT INTO lastval(id, mac, vnc) VALUES ('1', '244837814042624', '0')",
         "CREATE TABLE ifaces(id integer primary key autoincrement, "
             "vm_name char, if_name char, mac_addr char, ipv4_addr char, "
             "if_drv char, vhost integer, macvtap integer, parent_eth char, altname char)",

src/nm_edit_vm.c
@@ -88,7 +88,7 @@ void nm_edit_vm(const nm_str_t *name)
     if (nm_draw_form(action_window, form) != NM_OK)
         goto out;
 
-    nm_form_get_last(&last_mac, NULL);
+    last_mac = nm_form_get_last_mac();
 
     if (nm_edit_vm_get_data(&vm, &cur_settings) != NM_OK)
         goto out;
@@ -383,7 +383,7 @@ static void nm_edit_vm_update_db(nm_vm_t *vm, const nm_vmctl_data_t *cur, uint64
                 nm_str_t maddr = NM_INIT_STR;
                 mac++;
 
-                nm_net_mac_n2a(mac, &maddr);
+                nm_net_mac_n2s(mac, &maddr);
                 nm_str_format(&if_name, "%s_eth%zu",
                     nm_vect_str_ctx(&cur->main, NM_SQL_NAME), n);
                 nm_str_copy(&if_name_copy, &if_name);
@@ -410,8 +410,6 @@ static void nm_edit_vm_update_db(nm_vm_t *vm, const nm_vmctl_data_t *cur, uint64
                 nm_str_free(&if_name_copy);
                 nm_str_free(&maddr);
             }
-
-            nm_form_update_last_mac(mac);
         }
     }
 

src/nm_form.c
@@ -4,6 +4,7 @@
 #include <nm_vector.h>
 #include <nm_window.h>
 #include <nm_database.h>
+#include <nm_network.h>
 
 #include <time.h>
 #include <glob.h>
@@ -455,44 +456,33 @@ int nm_form_name_used(const nm_str_t *name)
     return rc;
 }
 
-void nm_form_get_last(uint64_t *mac, uint32_t *vnc)
+uint64_t nm_form_get_last_mac()
 {
+    uint64_t mac;
     nm_vect_t res = NM_INIT_VECT;
 
-    if (vnc != NULL)
-    {
-        nm_db_select("SELECT mac,vnc FROM lastval", &res);
-        *vnc = nm_str_stoui(res.data[1], 10);
-    }
-    else
-    {
-        nm_db_select("SELECT mac FROM lastval", &res);
-    }
+    nm_db_select("SELECT MAX(mac_addr) FROM ifaces", &res);
 
-    *mac = nm_str_stoul(res.data[0], 10);
+    mac = nm_net_mac_s2n(res.data[0]);
 
     nm_vect_free(&res, nm_str_vect_free_cb);
+
+    return mac;
 }
 
-void nm_form_update_last_mac(uint64_t mac)
+uint32_t nm_form_get_free_vnc()
 {
-    nm_str_t query = NM_INIT_STR;
+    uint32_t vnc;
+    nm_vect_t res = NM_INIT_VECT;
 
-    nm_str_format(&query, "UPDATE lastval SET mac='%" PRIu64 "'", mac);
-    nm_db_edit(query.data);
+    nm_db_select("\
+SELECT DISTINCT vnc + 1 FROM vms \
+UNION SELECT 0 EXCEPT SELECT DISTINCT vnc FROM vms \
+ORDER BY vnc ASC LIMIT 1", &res);
 
-    nm_str_free(&query);
-}
+    vnc = nm_str_stoui(res.data[0], 10);
 
-void nm_form_update_last_vnc(uint32_t vnc)
-{
-    nm_str_t query = NM_INIT_STR;
-
-    vnc++;
-    nm_str_format(&query, "UPDATE lastval SET vnc='%u'", vnc);
-    nm_db_edit(query.data);
-
-    nm_str_free(&query);
+    return vnc;
 }
 
 void nm_vm_free(nm_vm_t *vm)

src/nm_form.h
@@ -105,9 +105,8 @@ int nm_draw_form(nm_window_t *w, nm_form_t *form);
 void nm_form_free(nm_form_t *form, nm_field_t **fields);
 void nm_get_field_buf(nm_field_t *f, nm_str_t *res);
 int nm_form_name_used(const nm_str_t *name);
-void nm_form_get_last(uint64_t *mac, uint32_t *vnc);
-void nm_form_update_last_mac(uint64_t mac);
-void nm_form_update_last_vnc(uint32_t vnc);
+uint64_t nm_form_get_last_mac(void);
+uint32_t nm_form_get_free_vnc(void);
 int nm_print_empty_fields(const nm_vect_t *v);
 void nm_vm_free(nm_vm_t *vm);
 void nm_vm_free_boot(nm_vm_boot_t *vm);

src/nm_network.c
@@ -93,9 +93,7 @@ enum action {
     NM_SET_LINK_ADDR
 };
 
-#if defined (NM_OS_LINUX)
-static size_t nm_net_mac_a2n(const nm_str_t *addr, char *res, size_t len);
-#endif
+static size_t nm_net_mac_s2a(const nm_str_t *addr, char *res, size_t len);
 static void nm_net_manage_tap(const nm_str_t *name, int on_off);
 static void nm_net_addr_change(const nm_str_t *name, const nm_str_t *net,
                                int action);
@@ -146,7 +144,7 @@ void nm_net_add_macvtap(const nm_str_t *name, const nm_str_t *parent,
     req.i.ifi_family = AF_UNSPEC;
     req.i.ifi_flags |= IFF_UP | IFF_MULTICAST | IFF_ALLMULTI;
 
-    mac_len = nm_net_mac_a2n(maddr, macn, sizeof(macn));
+    mac_len = nm_net_mac_s2a(maddr, macn, sizeof(macn));
     if ((nm_net_add_attr(&req.n, sizeof(req), IFLA_ADDRESS,
             macn, mac_len) != NM_OK))
     {
@@ -434,7 +432,7 @@ int nm_net_fix_tap_name(nm_str_t *name, const nm_str_t *maddr)
     return 1;
 }
 
-void nm_net_mac_n2a(uint64_t maddr, nm_str_t *res)
+void nm_net_mac_n2s(uint64_t maddr, nm_str_t *res)
 {
     char buf[64] = {0};
     int pos = 0;
@@ -452,8 +450,7 @@ void nm_net_mac_n2a(uint64_t maddr, nm_str_t *res)
     nm_str_alloc_text(res, buf);
 }
 
-#if defined (NM_OS_LINUX)
-static size_t nm_net_mac_a2n(const nm_str_t *addr, char *res, size_t len)
+static size_t nm_net_mac_s2a(const nm_str_t *addr, char *res, size_t len)
 {
     nm_str_t copy = NM_INIT_STR;
     size_t n = 0;
@@ -484,7 +481,20 @@ static size_t nm_net_mac_a2n(const nm_str_t *addr, char *res, size_t len)
 
     return n + 1;
 }
-#endif /* NM_OS_LINUX */
+
+uint64_t nm_net_mac_s2n(const nm_str_t *addr)
+{
+    uint64_t mac = 0;
+    unsigned char buf[6];
+    const size_t buf_len = nm_arr_len(buf);
+
+    nm_net_mac_s2a(addr, (char *)buf, buf_len);
+
+    for (size_t i = 0; i < buf_len; ++i)
+        mac |= ((uint64_t)buf[i]) << 8 * (buf_len - 1 - i);
+
+    return mac;
+}
 
 static void nm_net_manage_tap(const nm_str_t *name, int on_off)
 {

commit 079a600d407a5b2c77649d63064c1d83910469b3

nm_string: Fix nm_str_append_format, broken in c218ea35e8b67d4adc1d66d3adb0b064feecacfe

src/nm_string.c
@@ -280,7 +280,7 @@ void nm_str_append_format(nm_str_t *str, const char *fmt, ...)
     buf = nm_calloc(1, len);
 
     va_start(args, fmt);
-    len = vsnprintf(buf, 0, fmt, args);
+    len = vsnprintf(buf, len, fmt, args);
     if (len < 0)
         nm_bug(_("%s: invalid length: %d"), __func__, len);
     va_end(args);