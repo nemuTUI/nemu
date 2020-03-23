86a3b1e8133e83e1e8b7c66980570e30e76314c7
Boris Stepanenko <boris.stepanenko@gmail.com>
2020-03-23 11:44:31 +0300

    rpm: added setup_nemu_nonroot.sh message

--- pkg/linux/rpm/postinstall.sh
+++ pkg/linux/rpm/postinstall.sh
@@ -3,6 +3,8 @@
 NEMU_COMP_SCRIPT="/usr/share/nemu/scripts/nemu.bash"
 COMP_DIR="/usr/share/bash-completion/completions"
 
+echo "Don't forget to run /usr/share/nemu/scripts/setup_nemu_nonroot.sh, if you want to use nEMU as non-root"
+
 if [ -d "$COMP_DIR" ]; then
   if [ ! -f "${COMP_DIR}/nemu" ]; then
     echo "Installing bash completion script in ${COMP_DIR}"
