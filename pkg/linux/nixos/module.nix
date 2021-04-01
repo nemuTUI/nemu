2fe0092428e26899acc676e101879217a43c0dbc
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-04-01 14:17:45 +0300

    pkg: added nixos package and system module

---
+++ pkg/linux/nixos/module.nix
@@ -0,0 +1,189 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.programs.nemu;
+
+in {
+  options.programs.nemu = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to add nemu to the global environment and configure a
+        setcap wrapper for it.
+      '';
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.nemu;
+      description = ''
+        The package to use.
+      '';
+    };
+
+    vhostNetGroup = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "vhost";
+      description = ''
+        Group for /dev/vhost-net. Will be created and udev rule will be added.
+      '';
+    };
+
+    macvtapGroup = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "vhost";
+      description = ''
+        Group for /dev/tapN. Will be created and udev rule will be added.
+      '';
+    };
+
+    usbGroup = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "usb";
+      description = ''
+        Group for USB devices. Will be created and udev rule will be added.
+      '';
+    };
+
+    users = mkOption {
+      default = {};
+      example = {
+        username_1 = {
+          autoAddVeth = true;
+          autoStartVMs = [ "vm1" "vm2" "vm3" ];
+        };
+        username_2 = {
+          autoAddVeth = true;
+          autoStartVMs = [ "vm" ];
+        };
+      };
+      description = ''
+        Users which will be able to run nemu.
+        They will be added to vhostNetGroup, macvtapGroup,
+        usbGroup and kvm group.
+      '';
+      type = types.attrsOf (types.submodule {
+        options = {
+          autoAddVeth = mkOption {
+            type = types.bool;
+            default = false;
+            description = ''
+              Whether to automaticaly create veth interfaces for user
+              during system startup.
+            '';
+          };
+
+          autoStartVMs = mkOption {
+            type = types.listOf types.str;
+            default = [];
+            example = [ "vm1" "vm2" "vm3" ];
+            description = ''
+              List of VMs which will be started automaticaly for user
+              during system statup.
+            '';
+          };
+        };
+      });
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ cfg.package ];
+
+    # Set capabilities
+    security.wrappers.nemu = {
+      source = "${cfg.package}/bin/nemu";
+      capabilities = "cap_net_admin+ep";
+    };
+
+    # Add vhost-net, macvtap and usb groups
+    users.groups =
+      optionalAttrs (isString cfg.vhostNetGroup) {
+        "${cfg.vhostNetGroup}" = { };
+      } // optionalAttrs (isString cfg.macvtapGroup) {
+        "${cfg.macvtapGroup}" = { };
+      } // optionalAttrs (isString cfg.usbGroup) {
+        "${cfg.usbGroup}" = { };
+      };
+
+    # Add nemu users to kvm, vhost-net, macvtap and usb groups
+    users.users = mapAttrs' (user: _:
+      nameValuePair "${user}" {
+        extraGroups = [
+          "kvm"
+        ]
+          ++ optional (isString cfg.vhostNetGroup) cfg.vhostNetGroup
+          ++ optional (isString cfg.macvtapGroup) cfg.macvtapGroup
+          ++ optional (isString cfg.usbGroup) cfg.usbGroup;
+      }
+    ) cfg.users;
+
+    # Add udev rules for vhost-net, macvtap and usb groups
+    services.udev.extraRules =
+      optionalString (isString cfg.vhostNetGroup) ''
+        KERNEL=="vhost-net", MODE="0660", GROUP="${cfg.vhostNetGroup}"
+      '' + optionalString (isString cfg.macvtapGroup) ''
+        SUBSYSTEM=="macvtap", MODE="0660", GROUP="${cfg.macvtapGroup}"
+      '' + optionalString (isString cfg.usbGroup) ''
+        SUBSYSTEM=="usb", MODE="0664", GROUP="${cfg.usbGroup}"
+      '';
+
+    # Add systemd nemu.target
+    systemd.targets.nemu = {
+      description = "nemu autostart target";
+      wantedBy = [ "multi-user.target" ];
+    };
+
+    # Add systemd nemu-veth.target
+    systemd.targets.nemu-veth = {
+      description = "nemu veth creation target";
+      before = [ "network-pre.target" ];
+      wantedBy = [ "nemu.target" ];
+    };
+
+    # Add systemd nemu-vm.target
+    systemd.targets.nemu-vm = {
+      description = "nemu autostart target";
+      wantedBy = [ "nemu.target" ];
+    };
+
+    # Add nemu-veth and nemu-vm systemd services for users
+    systemd.services = mapAttrs' (user: _:
+      nameValuePair "nemu-veth-${user}" {
+        description = "Creates veth interfaces for nemu VMs for ${user}";
+        serviceConfig = {
+          Type = "oneshot";
+          User = "${user}";
+          ExecStart = "${config.security.wrapperDir}/nemu -c";
+        };
+        wantedBy = [ "nemu-veth.target" ];
+      }
+    ) (filterAttrs (_: userOpts: userOpts.autoAddVeth == true) cfg.users)
+    // mapAttrs' (user: userOpts:
+      nameValuePair "nemu-vm-${user}" {
+        description = "Start nemu VMs for user ${user}";
+        serviceConfig = {
+          Type = "oneshot";
+          User = "${user}";
+          WorkingDirectory = "/home/${user}";
+          RemainAfterExit = "yes";
+          ExecStart = "${config.security.wrapperDir}/nemu --start "
+            + concatStringsSep "," userOpts.autoStartVMs;
+          ExecStop = "${config.security.wrapperDir}/nemu --poweroff "
+            + concatStringsSep "," userOpts.autoStartVMs;
+        };
+        after = [
+          "network-online.target"
+          "nemu-veth-${user}.service"
+        ];
+        wantedBy = [ "nemu-vm.target" ];
+      }
+    ) (filterAttrs (_: userOpts: userOpts.autoStartVMs != []) cfg.users);
+  };
+}
---
+++ pkg/linux/nixos/package.nix
@@ -0,0 +1,141 @@
+{ stdenv, config, lib, fetchFromGitHub, fetchpatch, installShellFiles
+, cmake
+, pkg-config
+, gettext
+, libpthreadstubs
+, libudev
+, libusb1
+, sqlite
+, qemu
+, ncurses
+, socat
+, picocom
+
+, dbus
+, graphviz
+, libxml2
+, libarchive
+, virtviewer
+, tigervnc
+
+, withDbus ? false
+, withNetworkMap ? false
+, withOVF ? true
+, withSnapshots ? false
+, withSpice ? true
+, withUTF ? false
+, withVNC ? true
+
+, configName ? ".config/nemu/nemu.cfg"
+, vmDir ? ".local/share/nemu/vms"
+, databaseName ? ".local/share/nemu/nemu.db"
+}:
+
+stdenv.mkDerivation rec {
+  pname = "nemu";
+  version = "2021-04-01";
+
+  src = fetchFromGitHub {
+    owner = "nemuTUI";
+    repo = "nemu";
+    rev = "2a7a84b57f5daef00a5abc291ed2ce70e471de62";
+    sha256 = "0if5ijkzkrxl3vs1vyapxfb3bga19ghykh5gdc1xg57di1wsnw10";
+  };
+
+  qemu_ = if withSnapshots
+    then qemu.overrideAttrs (attrs: {
+      patches = attrs.patches ++ [
+        (fetchpatch {
+          url = "https://raw.githubusercontent.com/nemuTUI/nemu/master/patches/qemu-qmp-savevm-5.0.0+.patch";
+          sha256 = "07w18h61m282f2nllxjxzv92lnvz61y9la6p8w7haj6a00kyispn";
+        })
+      ];
+    })
+    else qemu;
+
+  system.requiredKernelConfig = with config.lib.kernelConfig; [
+    (isEnabled "VETH")
+    (isEnabled "MACVTAP")
+  ];
+
+  nativeBuildInputs = [ cmake pkg-config installShellFiles ];
+
+  buildInputs = [
+    gettext
+    libpthreadstubs
+    libudev
+    libusb1
+    sqlite
+    qemu_
+    ncurses
+    socat
+    picocom
+  ]
+    ++ lib.optional withDbus dbus
+    ++ lib.optional withNetworkMap graphviz
+    ++ lib.optionals withOVF [ libxml2 libarchive ]
+    ++ lib.optional withSpice virtviewer
+    ++ lib.optional withVNC tigervnc;
+
+  cmakeFlags = [
+    "-DNM_CFG_NAME=${configName}"
+    "-DNM_DEFAULT_VMDIR=${vmDir}"
+    "-DNM_DEFAULT_VNC=${tigervnc}/bin/vncviewer"
+    "-DNM_DEFAULT_DBFILE=${databaseName}"
+    "-DNM_DEFAULT_SPICE=${virtviewer}/bin/remote-viewer"
+    "-DNM_DEFAULT_QEMUDIR=${qemu_}/bin"
+  ]
+    ++ lib.optional withDbus "-DNM_WITH_DBUS=ON"
+    ++ lib.optional withNetworkMap "-DNM_WITH_NETWORK_MAP=ON"
+    ++ lib.optional withOVF "-DNM_WITH_OVF_SUPPORT=ON"
+    ++ lib.optional withSnapshots "-DNM_SAVEVM_SNAPSHOTS=ON"
+    ++ lib.optional withSpice "-DNM_WITH_SPICE=ON"
+    ++ lib.optional withUTF "-DNM_USE_UTF=ON"
+    ++ lib.optional withVNC "-DNM_WITH_VNC_CLIENT=ON";
+
+  preConfigure = ''
+    patchShebangs .
+
+    substituteInPlace lang/ru/nemu.po --replace \
+      /bin/false /run/current-system/sw/bin/false
+
+    substituteInPlace nemu.cfg.sample --replace \
+      /usr/bin/vncviewer ${tigervnc}/bin/vncviewer
+
+    substituteInPlace nemu.cfg.sample --replace \
+      /usr/bin/remote-viewer ${virtviewer}/bin/remote-viewer
+
+    substituteInPlace nemu.cfg.sample --replace \
+      "qemu_bin_path = /usr/bin" "qemu_bin_path = ${qemu_}/bin"
+
+    substituteInPlace sh/ntty --replace \
+      /usr/bin/socat ${socat}/bin/socat
+
+    substituteInPlace sh/ntty --replace \
+      /usr/bin/picocom ${picocom}/bin/picocom
+
+    substituteInPlace sh/upgrade_db.sh --replace \
+      sqlite3 ${sqlite}/bin/sqlite3
+
+    substituteInPlace sh/setup_nemu_nonroot.sh --replace \
+      /usr/bin/nemu $out/bin/$pname
+
+    substituteInPlace src/nm_cfg_file.c --replace \
+      /bin/false /run/current-system/sw/bin/false
+  '';
+
+  preInstall = ''
+    install -D -m0644 -t $out/share/doc ../LICENSE
+  '';
+
+  postInstall = ''
+    installShellCompletion --bash $out/share/nemu/scripts/nemu.bash
+  '';
+
+  meta = {
+    homepage = "https://github.com/nemuTUI/nemu";
+    description = "Ncurses UI for QEMU";
+    license = lib.licenses.bsd2;
+    platforms = with lib.platforms; linux;
+  };
+}
