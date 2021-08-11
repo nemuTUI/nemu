6323e3ea26f4edf8c9dcc16aed8f03b80a43d59d
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-08-11 22:38:51 +0300

    nm_form: fix cppcheck errors

--- src/nm_form.c
+++ src/nm_form.c
@@ -125,6 +125,7 @@ nm_field_t *nm_field_default_new(int row, nm_form_data_t *form_data)
     return nm_field_new(NM_FIELD_DEFAULT, args, row, form_data);
 }
 
+/* cppcheck-suppress unusedFunction */
 nm_field_t *nm_field_alnum_new(int row, nm_form_data_t *form_data, int min_width)
 {
     nm_field_type_args_t args = {0};
@@ -132,6 +133,7 @@ nm_field_t *nm_field_alnum_new(int row, nm_form_data_t *form_data, int min_width
     return nm_field_new(NM_FIELD_ALNUM, args, row, form_data);
 }
 
+/* cppcheck-suppress unusedFunction */
 nm_field_t *nm_field_alpha_new(int row, nm_form_data_t *form_data, int min_width)
 {
     nm_field_type_args_t args = {0};
@@ -146,9 +148,9 @@ nm_field_t *nm_field_enum_new(
     nm_field_t *field;
     char **strings_ = NULL;
     nm_field_type_args_t args = {0};
-    size_t count = 0;
 
     if (strings) {
+        size_t count = 0;
         for (char **p = (char **)strings; *p; p++)
             count++;
 
@@ -181,6 +183,7 @@ nm_field_t *nm_field_integer_new(
     return nm_field_new(NM_FIELD_INTEGER, args, row, form_data);
 }
 
+/* cppcheck-suppress unusedFunction */
 nm_field_t *nm_field_numeric_new(
     int row, nm_form_data_t *form_data, int prec, double min, double max)
 {
