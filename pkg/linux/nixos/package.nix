302eae0c785da7fcd609c1f6d60a92d87c324eb7
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-08-31 10:44:26 +0300

    pkgs/linux/nixos: updated

--- pkg/linux/nixos/package.nix
+++ pkg/linux/nixos/package.nix
@@ -24,7 +24,6 @@
 , withNetworkMap ? false
 , withOVF ? true
 , withSpice ? true
-, withUnicodeGlyphs ? false
 , withVNC ? true
 , withRemote ? false
 
@@ -35,13 +34,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nemu";
-  version = "unstable-2021-08-27";
+  version = "unstable-2021-08-31";
 
   src = fetchFromGitHub {
     owner = "nemuTUI";
     repo = "nemu";
-    rev = "9d32b57fccf379b45f70d8c8a3baa0bbe7d7fe45";
-    sha256 = "1332ppylnil0q8kv72riv515jp05q9vkws3awihlzjg4kcchkg64";
+    rev = "c4fd6783b0b458d40b6d770ad47211e2f53be2c1";
+    sha256 = "1xgmbzgi0w21dwzz0yzla0g6ssk53gjga48mh2k0jyiczwlbjznh";
   };
 
   system.requiredKernelConfig = with config.lib.kernelConfig; [
@@ -82,7 +81,6 @@ stdenv.mkDerivation rec {
     ++ lib.optional withNetworkMap "-DNM_WITH_NETWORK_MAP=ON"
     ++ lib.optional withOVF "-DNM_WITH_OVF_SUPPORT=ON"
     ++ lib.optional withSpice "-DNM_WITH_SPICE=ON"
-    ++ lib.optional withUnicodeGlyphs "-DNM_WITH_UNICODE_GLYPHS=ON"
     ++ lib.optional withVNC "-DNM_WITH_VNC_CLIENT=ON"
     ++ lib.optional withRemote "-DNM_WITH_REMOTE=ON";
 
