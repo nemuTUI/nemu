1e9bb385669de4acfc0509c0ab96d3f69dc244a6
Pavel Balaev <mail@void.so>
2021-12-23 01:44:02 +0300

    test: add nemu and tmux classes

--- test/utils.py
+++ test/utils.py
@@ -1,9 +1,40 @@
 import os
+import uuid
+import time
+import shutil
 
 class Nemu():
-    def setup():
-        print("setup")
-        print(os.getenv("NEMU_BIN_DIR"))
+    def __init__(self):
+        self.uuid = uuid.uuid4().hex
+        self.test_dir = "/tmp/" + self.uuid
 
-    def cleanup():
-        print("cleanup")
+        os.mkdir(self.test_dir)
+        with open(self.test_dir + "/nemu.cfg", 'w') as out:
+            with open("nemu.cfg.in", 'r') as input:
+                for line in input:
+                    out.write(line.replace("%%DIR%%", self.uuid))
+
+    def cleanup(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_dir(self):
+        return self.test_dir
+
+class Tmux():
+    def __init__(self, path):
+        self.uuid = uuid.uuid4().hex
+        cmd = "tmux -L " + self.uuid + " new-session -d " + \
+                os.getenv("NEMU_BIN_DIR") + "/nemu --cfg " + path + "/nemu.cfg"
+        print(cmd)
+        os.popen(cmd)
+        time.sleep(0.5)
+
+    def send(self, key):
+        cmd = "tmux -L " + self.uuid + " send-keys " + key
+        os.popen(cmd)
+        time.sleep(0.5)
+
+    def cleanup(self):
+        cmd = "tmux -L " + self.uuid + " kill-server"
+        os.popen(cmd)
+        time.sleep(0.5)
--- test/vm_test.py
+++ test/vm_test.py
@@ -1,9 +1,22 @@
 import unittest
 from utils import Nemu
+from utils import Tmux
 
 class TestVm(unittest.TestCase):
     def test_install(self):
         """Test VM install"""
-        Nemu.setup()
+        nemu = Nemu()
         self.assertTrue(1 == 1)
-        Nemu.cleanup()
+        tmux = Tmux(nemu.test_dir)
+        tmux.send("I")
+        tmux.send("testvm")
+        for i in range (3):
+            tmux.send("Down")
+        tmux.send("256")
+        tmux.send("Down")
+        tmux.send("10")
+        for i in range (3):
+            tmux.send("Down")
+        tmux.send("/dev/null.iso")
+        tmux.send("Enter")
+        nemu.cleanup()
