1b514fc259fdd45ce1cc54e1eeb1cf5996cf1c45
Pavel Balaev <mail@void.so>
2021-12-29 11:30:12 +0300

    tests: fix template replace marker

--- test/vm_test.py
+++ test/vm_test.py
@@ -5,22 +5,22 @@ from utils import Tmux
 
 class TestVm(unittest.TestCase):
     def test_install(self):
-        self.maxDiff = None
         """Test VM install"""
+        self.maxDiff = None
         tmpl = "/usr/bin/qemu-system-x86_64 -daemonize -usb -device \
 qemu-xhci,id=usbbus -boot d -cdrom /dev/null.iso -drive \
 node-name=hd0,media=disk,if=virtio,file=\
-/tmp/%%DIR%%/testvm/testvm_a.img \
+/tmp/%%UUID%%/testvm/testvm_a.img \
 -m 256 -enable-kvm -cpu host -M pc-i440fx-6.0 -device \
 virtio-net-pci,mac=de:ad:be:ef:00:01,netdev=netdev0 -netdev \
 tap,ifname=testvm_eth0,script=no,downscript=no,id=netdev0,\
-vhost=on -pidfile /tmp/%%DIR%%/testvm/qemu.pid -qmp \
-unix:/tmp/%%DIR%%/testvm/qmp.sock,server,nowait -vga qxl \
+vhost=on -pidfile /tmp/%%UUID%%/testvm/qemu.pid -qmp \
+unix:/tmp/%%UUID%%/testvm/qmp.sock,server,nowait -vga qxl \
 -spice port=5900,disable-ticketing=on"
         nemu = Nemu()
         tmux = Tmux()
         tmux.setup(nemu.test_dir)
-        tmpl = tmpl.replace("%%DIR%%", nemu.uuid)
+        tmpl = tmpl.replace("%%UUID%%", nemu.uuid)
         rc = tmux.send("I")
         self.assertTrue(0 == rc)
         rc = tmux.send("testvm")
