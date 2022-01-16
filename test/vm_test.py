79802cfbc2da7cc826132c06f83a82497786693b
Pavel Balaev <balaev@tarantool.org>
2022-01-17 00:17:47 +0300

    test: vm edit settings

--- test/vm_test.py
+++ test/vm_test.py
@@ -1,6 +1,5 @@
 import unittest
 import subprocess
-import time
 from utils import Nemu
 from utils import Tmux
 
@@ -14,8 +13,7 @@ class TestVm(unittest.TestCase):
         keys = [["I"], ["testvm"], ["Down", 3], ["256"], ["Down"],
                 ["10"], ["Down", 3], ["/dev/null.iso"], ["Enter"], ["q"]]
         for key in keys:
-            repeate = key[1] if len(key) == 2 else 1
-            rc = tmux.send(key[0], repeate)
+            rc = tmux.send(*key)
             self.assertTrue(0 == rc)
 
         expected = f"{nemu.qemu_bin()}/qemu-system-x86_64 -daemonize -usb -device \
@@ -40,26 +38,28 @@ unix:/tmp/{nemu.uuid}/testvm/qmp.sock,server,nowait -vga qxl \
         keys = [["I"], ["testvm"], ["Down", 3], ["256"], ["Down"],
                 ["10"], ["Down", 3], ["/dev/null.iso"], ["Enter"], ["q"]]
         for key in keys:
-            repeate = key[1] if len(key) == 2 else 1
-            rc = tmux_init.send(key[0], repeate)
+            rc = tmux_init.send(*key)
             self.assertTrue(0 == rc)
 
         tmux_edit = Tmux()
         tmux_edit.setup(nemu.test_dir)
-        keys_edit = [["e"], ["10"], ["Down"], ["BSpace", 3], ["512"], ["Enter"], ["q"]]
+        keys_edit = [["e"], ["10"], ["Down"], ["BSpace", 3], ["512"],
+                ["Down"], ["Right"], ["Down"], ["Right"], ["Down"], ["Left"],
+                ["2"], ["Down"], ["Right"], ["Enter"], ["q"]]
         for key in keys_edit:
-            repeate = key[1] if len(key) == 2 else 1
-            rc = tmux_edit.send(key[0], repeate)
+            rc = tmux_edit.send(*key)
             self.assertTrue(0 == rc)
 
         expected = f"{nemu.qemu_bin()}/qemu-system-x86_64 -daemonize -usb -device \
 qemu-xhci,id=usbbus -boot d -cdrom /dev/null.iso -drive \
-node-name=hd0,media=disk,if=virtio,file=\
+node-name=hd0,media=disk,if=ide,file=\
 /tmp/{nemu.uuid}/testvm/testvm_a.img \
--m 512 -smp 10 -enable-kvm -cpu host -M {nemu.qemu_mtype()} -device \
+-m 512 -smp 10 -M {nemu.qemu_mtype()} -device \
 virtio-net-pci,mac=de:ad:be:ef:00:01,netdev=netdev0 -netdev \
 tap,ifname=testvm_eth0,script=no,downscript=no,id=netdev0,\
-vhost=on -pidfile /tmp/{nemu.uuid}/testvm/qemu.pid -qmp \
+vhost=on -device virtio-net-pci,mac=de:ad:be:ef:00:02,netdev=netdev1 \
+-netdev tap,ifname=testvm_eth1,script=no,downscript=no,id=netdev1 \
+-pidfile /tmp/{nemu.uuid}/testvm/qemu.pid -qmp \
 unix:/tmp/{nemu.uuid}/testvm/qmp.sock,server,nowait -vga qxl \
 -spice port=5900,disable-ticketing=on"
         self.assertEqual(nemu.result("testvm"), expected)
