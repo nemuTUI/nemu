55e27623c0acab174a73b0ebbd6915353c757837
Pavel Balaev <mail@void.so>
2022-01-24 15:42:37 +0300

    test: clone vm

--- test/vm_test.py
+++ test/vm_test.py
@@ -64,3 +64,35 @@ unix:/tmp/{nemu.uuid}/testvm/qmp.sock,server,nowait -vga qxl \
 -spice port=5900,disable-ticketing=on"
         self.assertEqual(nemu.result("testvm"), expected)
         nemu.cleanup()
+
+    def test_03_clone(self):
+        """Test clone VM"""
+        self.maxDiff = None
+        nemu = Nemu()
+        tmux_init = Tmux()
+        tmux_init.setup(nemu.test_dir)
+        keys = [["I"], ["testvm"], ["Down", 3], ["256"], ["Down"],
+                ["10"], ["Down", 3], ["/dev/null.iso"], ["Enter"], ["q"]]
+        for key in keys:
+            rc = tmux_init.send(*key)
+            self.assertTrue(0 == rc)
+
+        tmux_edit = Tmux()
+        tmux_edit.setup(nemu.test_dir)
+        keys_edit = [["l"], ["Enter"], ["q"]]
+        for key in keys_edit:
+            rc = tmux_edit.send(*key)
+            self.assertTrue(0 == rc)
+
+        expected = f"{nemu.qemu_bin()}/qemu-system-x86_64 -daemonize -usb -device \
+qemu-xhci,id=usbbus -boot d -cdrom /dev/null.iso -drive \
+node-name=hd0,media=disk,if=virtio,file=\
+/tmp/{nemu.uuid}/testvm-clone/testvm-clone_a.img \
+-m 256 -enable-kvm -cpu host -M {nemu.qemu_mtype()} -device \
+virtio-net-pci,mac=de:ad:be:ef:00:02,netdev=netdev0 -netdev \
+tap,ifname=vm-deadbeef0002,script=no,downscript=no,id=netdev0,\
+vhost=on -pidfile /tmp/{nemu.uuid}/testvm-clone/qemu.pid -qmp \
+unix:/tmp/{nemu.uuid}/testvm-clone/qmp.sock,server,nowait -vga qxl \
+-spice port=5901,disable-ticketing=on"
+        self.assertEqual(nemu.result("testvm-clone"), expected)
+        nemu.cleanup()
